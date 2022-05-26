8e7637f533ec2fc29fa78ae09552bb37208dcaa5
Pedro Harmendani <pedro.harmendani@avanade.com>
2022-05-26 17:57:53 -0300

    Add error testing api.

--- tests/api.js
+++ tests/api.js
@@ -1,17 +1,23 @@
 'use strict';
 
+const {expect} = require('chai');
 const request = require('supertest');
 
 describe('Integration API Tests', () => {
   context('Success on Response', async () => {
-    it('should return contents from search', (done) => {
-      request('https://de1.api.radio-browser.info')
-        .get('/json/stations?limit=10')
-        .send()
-        .expect(200)
-        .end((error) => { 
-            done(error);          
-        });
+    it('should returns a radio streaming list with 200 HTTP STATUS CODE', async() => {
+      const response = await request('https://de1.api.radio-browser.info')
+        .get('/json/stations?limit=10').send();
+      expect(response.statusCode).to.equal(200);
+      expect(response).to.be.an('array');
+    });
+  });
+  context('Error on Response', async () => {
+    it('should returns a 404 HTTP Status Code', async() => {
+      const response = await request('https://de1.api.radio-browser.info')
+        .get('/json/teste').send();
+      expect(response.statusCode).to.equal(404);
+      expect(response).to.be.undefined;
     });
   });
 });
